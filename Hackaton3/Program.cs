2e742194f7f2b4c0fba94d34fd38eb44b9234f1c
OscarJRIvera <70442896+OscarJRIvera@users.noreply.github.com>
2022-11-05 16:00:49 -0600

    finalv2

--- Hackaton3/Program.cs
+++ Hackaton3/Program.cs
@@ -6,7 +6,10 @@ namespace HackatonProblema3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("No se entendio el problema");
+            Console.WriteLine("ingresa priemra cadena");
+            string C1I = Console.ReadLine();
+            Console.WriteLine("ingresa segunda cadena ");
+            string C2I = Console.ReadLine();
         }
     }
 }
